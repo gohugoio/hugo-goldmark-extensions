e2e91c74b636b7ab464945e886e23c770515ca36
Jeremy Kun <j2kun@users.noreply.github.com>
2024-01-11 14:06:10 -0800

    Implement inline parser

--- passthrough/passthrough.go
+++ passthrough/passthrough.go
@@ -1,6 +1,124 @@
 package passthrough
 
-// Foo returns a string.
-func Foo() string {
-	return "foo"
+import (
+	"bytes"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/text"
+	"github.com/yuin/goldmark/util"
+)
+
+type delimiters struct {
+	Open  string
+	Close string
+}
+
+type inlinePassthroughParser struct {
+	PassthroughDelimiters []delimiters
+}
+
+// NewInlinePassthroughParser return a new InlineParser that parses inline pass through.
+func NewInlinePassthroughParser(ds []delimiters) parser.InlineParser {
+	return &inlinePassthroughParser{
+		PassthroughDelimiters: ds,
+	}
+}
+
+// `Parse` will be executed once for each character that is in this list of
+// allowed trigger characters. Our parse function needs to do some additional
+// checks because Trigger only works for single-byte delimiters.
+func (s *inlinePassthroughParser) Trigger() []byte {
+	var firstBytes []byte
+
+	for _, d := range s.PassthroughDelimiters {
+		firstBytes = append(firstBytes, d.Open[0])
+		if d.Open[0] != d.Close[0] {
+			firstBytes = append(firstBytes, d.Close[0])
+		}
+	}
+
+	return firstBytes
+}
+
+// Determine if a byte array starts with a given string
+func startsWith(b []byte, s string) bool {
+	if len(b) < len(s) {
+		return false
+	}
+
+	return string(b[:len(s)]) == s
+}
+
+// Determine if the input slice starts with a full valid opening delimiter.
+// If so, returns the delimiter struct, otherwise returns nil.
+func (s *inlinePassthroughParser) GetFullOpeningDelimiter(line []byte) *delimiters {
+	for _, d := range s.PassthroughDelimiters {
+		if startsWith(line, d.Open) {
+			return &d
+		}
+	}
+
+	return nil
+}
+
+func (s *inlinePassthroughParser) Parse(parent ast.Node, block text.Reader, pc parser.Context) ast.Node {
+	// In order to prevent other parser extensions from operating on the text
+	// between passthrough delimiters, we must process the entire inline
+	// passthrough in one execution of Parse. This means we can't use the style
+	// of multiple triggers with parser.Context state saved between calls.
+	line, startSegment := block.PeekLine()
+
+	fencePair := s.GetFullOpeningDelimiter(line)
+	// fencePair == nil can happen if only the first byte of an opening delimiter
+	// matches, but it is not the complete opening delimiter. The trigger causes
+	// this Parse function to execute, but the trigger interface is limited to
+	// matching single bytes.
+	if fencePair == nil {
+		return nil
+	}
+
+	// This roughly follows goldmark/parser/code_span.go
+	block.Advance(len(fencePair.Open))
+	openerSize := len(fencePair.Open)
+	l, pos := block.Position()
+
+	for {
+		line, lineSegment := block.PeekLine()
+		if line == nil {
+			block.SetPosition(l, pos)
+			return ast.NewTextSegment(startSegment.WithStop(startSegment.Start + openerSize))
+		}
+
+		closingDelimiterPos := bytes.Index(line, []byte(fencePair.Close))
+		if closingDelimiterPos == -1 { // no closer on this line
+			block.AdvanceLine()
+			continue
+		}
+
+		// This segment spans from the original starting trigger (including the delimiter)
+		// up to and including the closing delimiter.
+		seg := startSegment.WithStop(lineSegment.Start + closingDelimiterPos + len(fencePair.Close))
+		block.Advance(closingDelimiterPos + len(fencePair.Close))
+
+		return ast.NewRawTextSegment(seg)
+	}
+}
+
+type passthrough struct {
+	InlineDelimiters []delimiters
+	BlockDelimiters  []delimiters
+}
+
+func NewPassthroughWithDelimiters(Inlines []delimiters, Blocks []delimiters) goldmark.Extender {
+	return &passthrough{
+		InlineDelimiters: Inlines,
+		BlockDelimiters:  Blocks,
+	}
+}
+
+func (e *passthrough) Extend(m goldmark.Markdown) {
+	m.Parser().AddOptions(parser.WithInlineParsers(
+		util.Prioritized(NewInlinePassthroughParser(e.InlineDelimiters), 501),
+	))
 }
--- passthrough/passthrough_test.go
+++ passthrough/passthrough_test.go
@@ -1,12 +1,118 @@
 package passthrough
 
 import (
+	"bytes"
+	"strings"
 	"testing"
+
+	"github.com/yuin/goldmark"
 
 	qt "github.com/frankban/quicktest"
 )
 
-func TestFoo(t *testing.T) {
+func buildTestParser() goldmark.Markdown {
+	md := goldmark.New(
+		goldmark.WithExtensions(NewPassthroughWithDelimiters(
+			/*inlines*/ []delimiters{
+				{
+					Open:  "$",
+					Close: "$",
+				},
+				{
+					Open:  "\\(",
+					Close: "\\)",
+				},
+			},
+			/*blocks*/ []delimiters{
+				{
+					Open:  "$$",
+					Close: "$$",
+				},
+				{
+					Open:  "\\[",
+					Close: "\\[",
+				},
+			},
+		)),
+	)
+	return md
+}
+
+func Parse(t *testing.T, input string) string {
+	md := buildTestParser()
+	var buf bytes.Buffer
+	if err := md.Convert([]byte(input), &buf); err != nil {
+		t.Fatal(err)
+	}
+	return strings.TrimSpace(buf.String())
+}
+
+func TestEmphasisOutsideOfMathmode(t *testing.T) {
+	input := "Emph: _wow_"
+	expected := "<p>Emph: <em>wow</em></p>"
+	actual := Parse(t, input)
+
 	c := qt.New(t)
-	c.Assert(Foo(), qt.Equals, "foo")
+	c.Assert(actual, qt.Equals, expected)
+}
+
+func TestInlineEquationWithEmphasisDelimiters(t *testing.T) {
+	input := "An equation: $a^*=x-b^*$. Amazing"
+	expected := "<p>An equation: $a^*=x-b^*$. Amazing</p>"
+	actual := Parse(t, input)
+
+	c := qt.New(t)
+	c.Assert(actual, qt.Equals, expected)
+}
+
+func TestInlineEquationWithMultiByteDelimiters(t *testing.T) {
+	input := "An equation: \\(a^*=x-b^*\\). Amazing"
+	expected := "<p>An equation: \\(a^*=x-b^*\\). Amazing</p>"
+	actual := Parse(t, input)
+
+	c := qt.New(t)
+	c.Assert(actual, qt.Equals, expected)
+}
+
+func TestInlineEquationWithEmphasisDelimitersSplitAcrossLines(t *testing.T) {
+	input := `An equation: $a^*=
+x-b^*$. Amazing`
+	expected := `<p>An equation: $a^*=
+x-b^*$. Amazing</p>`
+	actual := Parse(t, input)
+
+	c := qt.New(t)
+	c.Assert(actual, qt.Equals, expected)
+}
+
+func TestInlineEquationWithEmphasisSplitAcrossParagraphs(t *testing.T) {
+	input := `An equation: $a^
+
+*=x-b^*$. Amazing`
+	expected := `<p>An equation: $a^</p>
+<p><em>=x-b^</em>$. Amazing</p>`
+	actual := Parse(t, input)
+
+	c := qt.New(t)
+	c.Assert(actual, qt.Equals, expected)
+}
+
+func TestBlockEquationWithEmphasisDelimiters(t *testing.T) {
+	input := `An equation:
+
+$$
+a^*=x-b^*
+$$
+
+Amazing`
+	expected := `<p>An equation:</p>
+<p>$$
+a^*=x-b^*
+$$</p>
+<p>Amazing</p>`
+
+	actual := Parse(t, input)
+
+	c := qt.New(t)
+	c.Assert(actual, qt.Equals, expected)
 }
