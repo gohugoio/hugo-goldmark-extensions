827e8e8aa3e0eb2b657e6d42d733230bbffb53e4
Jeremy Kun <j2kun@users.noreply.github.com>
2024-01-20 19:24:03 -0800

    Apply patch to update tests
    From https://github.com/gohugoio/hugo-goldmark-extensions/pull/2#issuecomment-1902300812

--- passthrough/passthrough_test.go
+++ passthrough/passthrough_test.go
@@ -187,8 +187,9 @@ func TestExample1(t *testing.T) {
 }
 
 func TestExample2(t *testing.T) {
-	input := `x = {-b \pm \sqrt{b^2-4ac} \over 2a}
-n$ equation`
+	input := `Inline $
+x = {-b \pm \sqrt{b^2-4ac} \over 2a}
+$ equation`
 
 	expected := "<p>" + input + "</p>"
 	actual := Parse(t, input)
@@ -482,22 +483,22 @@ $$`
 
 func TestExample26(t *testing.T) {
 	input := `\[
- \begin{array} {lcl}
- 	L(p,w_i) &=& \dfrac{1}{N}\Sigma_{i=1}^N(\underbrace{f_r(x_2
- 	\rightarrow x_1
- 	\rightarrow x_0)G(x_1
- 	\longleftrightarrow x_2)f_r(x_3
- 	\rightarrow x_2
- 	\rightarrow x_1)}_{sample\, radiance\, evaluation\, in\, stage2}
- 	\\\\\\ &=&
- 	\prod_{i=3}^{k-1}(\underbrace{\dfrac{f_r(x_{i+1}
- 	\rightarrow x_i
- 	\rightarrow x_{i-1})G(x_i
- 	\longleftrightarrow x_{i-1})}{p_a(x_{i-1})}}_{stored\,in\,vertex\, during\,light\, path\, tracing\, in\, stage1})\dfrac{G(x_k
- 	\longleftrightarrow x_{k-1})L_e(x_k
- 	\rightarrow x_{k-1})}{p_a(x_{k-1})p_a(x_k)})
- \end{array}
- \]`
+\begin{array} {lcl}
+L(p,w_i) &=& \dfrac{1}{N}\Sigma_{i=1}^N(\underbrace{f_r(x_2
+\rightarrow x_1
+\rightarrow x_0)G(x_1
+\longleftrightarrow x_2)f_r(x_3
+\rightarrow x_2
+\rightarrow x_1)}_{sample\, radiance\, evaluation\, in\, stage2}
+\\\\\\ &=&
+\prod_{i=3}^{k-1}(\underbrace{\dfrac{f_r(x_{i+1}
+\rightarrow x_i
+\rightarrow x_{i-1})G(x_i
+\longleftrightarrow x_{i-1})}{p_a(x_{i-1})}}_{stored\,in\,vertex\, during\,light\, path\, tracing\, in\, stage1})\dfrac{G(x_k
+\longleftrightarrow x_{k-1})L_e(x_k
+\rightarrow x_{k-1})}{p_a(x_{k-1})p_a(x_k)})
+\end{array}
+\]`
 
 	expected := input
 	actual := Parse(t, input)
